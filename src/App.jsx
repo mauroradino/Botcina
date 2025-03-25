d579f980ce2828ecc7be367ad6b9d120a740b1a7
mauroradino <mauroradino2003@gmail.com>
2025-03-25 13:21:01 -0300

    docker changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
    setRecetas("")
    setLoading(true)
    try {
-     const response = await fetch('http://127.0.0.1:8001/recipes', {
+     const response = await fetch('http://127.0.0.1:8002/recipes', {
        method: 'POST',
        headers: {
          'Content-Type': 'application/json',
